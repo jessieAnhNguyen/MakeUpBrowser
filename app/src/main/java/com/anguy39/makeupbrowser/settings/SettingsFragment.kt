725ef44f3800e06b4e68944ec4f8e83693e400f8
Jessie Anh Nguyen <phuonganhnguyenthi26.3@gmail.com>
2021-04-30 15:34:29 -0400

    designed preference fragment for settings

---
+++ app/src/main/java/com/anguy39/makeupbrowser/settings/SettingsFragment.kt
@@ -0,0 +1,20 @@
+package com.anguy39.makeupbrowser.settings
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import com.anguy39.makeupbrowser.R
+import androidx.preference.PreferenceFragmentCompat
+
+class SettingsFragment : PreferenceFragmentCompat() {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+//        setHasOptionsMenu(true)
+        setPreferencesFromResource(R.xml.settings_preference, rootKey)
+    }
+
+//    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+//        super.onCreateOptionsMenu(menu, inflater)
+//        menu.clear()
+//    }
+}
