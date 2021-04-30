2bb06ec461a532f43c5383de721e8a55b8051491
Jessie Anh Nguyen <phuonganhnguyenthi26.3@gmail.com>
2021-04-29 20:59:07 -0400

    implemented info fragment details

--- app/src/main/java/com/anguy39/makeupbrowser/info/InfoFragment.kt
+++ app/src/main/java/com/anguy39/makeupbrowser/info/InfoFragment.kt
@@ -5,16 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.anguy39.makeupbrowser.BuildConfig
 import com.anguy39.makeupbrowser.R
+import com.anguy39.makeupbrowser.databinding.FragmentInfoBinding
 
 class InfoFragment : Fragment() {
+
+    var binding: FragmentInfoBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_info, container, false)
+        val infoFragmentBinding = FragmentInfoBinding.inflate(inflater, container, false)
+        binding = infoFragmentBinding
+        return infoFragmentBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding?.apply {
+            appNameTextView.text = resources.getString(R.string.app_name)
+            appIDTextView.text = BuildConfig.APPLICATION_ID
+            versionNumberTextView.text = BuildConfig.VERSION_NAME
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
     }
 
 
